5c3ccca05bf5ca8e06a3ea6fc919b3c51c1aae6a
Jeremy Kahn <jeremyckahn@gmail.com>
2015-08-20 13:30:16 -0700

    Don't render analytics code

--- app/scripts/components/control-panel/main.js
+++ app/scripts/components/control-panel/main.js
@@ -57,7 +57,7 @@ define([
       }, {
         modelAttributes: {
           cssExportClass: 'mantra'
-          ,analyticsUrl: 'FIXME'
+          ,analyticsUrl: ''
           ,enableOrientationControls: false
         }
       });
