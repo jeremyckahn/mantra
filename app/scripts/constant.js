46b741f5e5d51c7988d004b6f9c173708f895857
Jeremy Kahn <jeremyckahn@gmail.com>
2015-09-12 15:04:09 -0500

    Specify default values for supported properties

--- app/scripts/constant.js
+++ app/scripts/constant.js
@@ -4,17 +4,17 @@ define(function () {
   return {
     TRANSIENT_TIMELINE_NAME: '__transientTimeline'
     ,SUPPORTED_PROPERTIES: [
-      'opacity'
-      ,'rotateX'
-      ,'rotateY'
-      ,'rotateZ'
-      ,'scale'
-      ,'scaleX'
-      ,'scaleY'
-      ,'skewX'
-      ,'skewY'
-      ,'translateX'
-      ,'translateY'
+      {  name: 'opacity', defaultValue: 0 }
+      ,{ name: 'rotateX', defaultValue: '0deg' }
+      ,{ name: 'rotateY', defaultValue: '0deg' }
+      ,{ name: 'rotateZ', defaultValue: '0deg' }
+      ,{ name: 'scale', defaultValue: 0 }
+      ,{ name: 'scaleX', defaultValue: 0 }
+      ,{ name: 'scaleY', defaultValue: 0 }
+      ,{ name: 'skewX', defaultValue: 0 }
+      ,{ name: 'skewY', defaultValue: 0 }
+      ,{ name: 'translateX', defaultValue: '0px' }
+      ,{ name: 'translateY', defaultValue: '0px' }
     ]
   };
 });
